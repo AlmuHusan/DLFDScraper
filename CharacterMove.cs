11e980d7bb25511474b84411901109fc9b2a4176
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2019-12-19 13:46:41 -0500

    update 1

---
+++ CharacterMove.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace DustloopFDScraper
+{
+    class CharacterMove
+    {
+        [JsonPropertyName("character")]
+        public string character { get; set; }
+
+        [JsonPropertyName("move")]
+        public string move { get; set; }
+
+        [JsonPropertyName("damage")]
+        public string damage { get; set; }
+
+        [JsonPropertyName("cancel")]
+        public string cancel { get; set; }
+
+        [JsonPropertyName("p1")]
+        public string p1 { get; set; }
+
+        [JsonPropertyName("p2")]
+        public string p2 { get; set; }
+
+        [JsonPropertyName("attribute")]
+        public string attribute { get; set; }
+
+        [JsonPropertyName("guard")]
+        public string guard { get; set; }
+
+        [JsonPropertyName("startup")]
+        public string startup { get; set; }
+
+        [JsonPropertyName("active")]
+        public string active { get; set; }
+
+        [JsonPropertyName("recovery")]
+        public string recovery { get; set; }
+
+        [JsonPropertyName("franeAdv")]
+        public string frameAdv { get; set; }
+
+        [JsonPropertyName("level")]
+        public string level { get; set; }
+
+        [JsonPropertyName("blockstun")]
+        public string blockstun { get; set; }
+
+        [JsonPropertyName("hitstun")]
+        public string hitstun { get; set; }
+
+        [JsonPropertyName("untech")]
+        public string untech { get; set; }
+
+        [JsonPropertyName("hitstop")]
+        public string hitstop { get; set; }
+
+        [JsonPropertyName("invul")]
+        public string invul { get; set; }
+
+        [JsonPropertyName("hitbox")]
+        public string hitbox { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Xml;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DustloopFDScraper
 {
@@ -10,44 +11,144 @@ namespace DustloopFDScraper
     {
         static void Main(string[] args)
         {
-            var html = @"http://www.dustloop.com/wiki/index.php?title=BBTag/Frame_Data";
-
+            var frameDataPage = @"http://www.dustloop.com/wiki/index.php?title=BBTag/Frame_Data";
+            var dustloopPage = @"http://www.dustloop.com";
             HtmlWeb web = new HtmlWeb();
 
-            var htmlDoc = web.Load(html);
-            var nodes = htmlDoc.DocumentNode.SelectNodes("//body//a");
+            var FrameDataHtmlDoc = web.Load(frameDataPage);
+            var FrameDataNodes = FrameDataHtmlDoc.DocumentNode.SelectNodes("//body//a");
             Regex characterNameSearch = new Regex(@"(?<=\/wiki\/index\.php\?title=BBTag\/).+(?=\/Frame_Data)");
             var characterFDLinks = new Dictionary<string, string>();
             Regex linkSearch = new Regex(@"/wiki/index.php\?title=BBTag/.+/Frame_Data");
-            foreach (var node in nodes) {
+            foreach (var node in FrameDataNodes)
+            {
                 try
                 {
-                    
+
                     Match linkMatch = linkSearch.Match(node.Attributes["href"].Value);
-                    
+
                     //Console.WriteLine(imageMatch.Value);
                     if (linkMatch.Value != "")
                     {
-                        Console.WriteLine(linkMatch.Value);
-                        Match characterName= characterNameSearch.Match(linkMatch.Value);
-                        Console.WriteLine(characterName.Value);
+                        //Console.WriteLine(linkMatch.Value);
+                        Match characterName = characterNameSearch.Match(linkMatch.Value);
+                        //Console.WriteLine(characterName.Value);
                         try
                         {
-                            characterFDLinks.Add(linkMatch.Value, characterName.Value);
+                            characterFDLinks.Add(characterName.Value, linkMatch.Value);
                         }
                         catch (ArgumentException)
                         {
-                           // Console.WriteLine("An element with Key = \"txt\" already exists.");
+                            // Console.WriteLine("An element with Key = \"txt\" already exists.");
                         }
                     }
                     //Console.WriteLine(node.Attributes["href"].Value);
                 }
                 catch (Exception e) { }
             }
-            Console.WriteLine("HID");
-            foreach (var c in characterFDLinks.Keys) {
+
+            foreach (var c in characterFDLinks.Keys)
+            {
                 Console.WriteLine(c);
+                Console.WriteLine(dustloopPage + characterFDLinks[c]);
+
             }
+            var CharacterDataHtmlDoc = web.Load(dustloopPage + characterFDLinks["Hazama"]);
+            var CharacterDataNodes = CharacterDataHtmlDoc.DocumentNode.SelectNodes("//tr[count(td) >12]/*");
+            int dataTypeNumber = 1;
+            List<CharacterMove> moves = new List<CharacterMove>();
+            CharacterMove characterMove = new CharacterMove();
+            foreach (var CharacterDataNode in CharacterDataNodes)
+            {
+                Console.WriteLine(CharacterDataNode.InnerText);
+                switch (dataTypeNumber)
+                {
+                    case 1:
+                        characterMove.character = "Hazama";
+                        characterMove.move = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 2:
+                        characterMove.damage = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 3:
+                        characterMove.cancel = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 4:
+                        characterMove.p1 = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 5:
+                        characterMove.p2 = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 6:
+                        characterMove.attribute = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 7:
+                        characterMove.guard = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 8:
+                        characterMove.startup = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 9:
+                        characterMove.active = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 10:
+                        characterMove.recovery = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 11:
+                        characterMove.frameAdv = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 12:
+                        characterMove.level = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 13:
+                        characterMove.blockstun = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 14:
+                        characterMove.hitstun = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 15:
+                        characterMove.untech = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 16:
+                        characterMove.hitstop = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 17:
+                        characterMove.invul = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber++;
+                        break;
+                    case 18:
+                        characterMove.hitbox = CharacterDataNode.InnerText.Trim();
+                        dataTypeNumber = 1;
+                        moves.Add(characterMove);
+                        characterMove = new CharacterMove();
+                        break;
+                    default:
+                        Console.WriteLine("Something went wrong");
+                        break;
+
+                }
+
+            }
+            
+            var json = JsonConvert.SerializeObject(moves.ToArray());
+            Console.WriteLine(json.ToString());
+            System.IO.File.WriteAllText(@"C:\Users\owner\Desktop\CIS467\testjson.txt", json);
         }
     }
 }
